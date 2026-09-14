66e46250c4e602c411206b5a6fe60fdbb59e8aa3
yharsh12 <harshyadav121204@gmail.com>
2026-09-15 02:05:05 +0530

    Update judge0.js

--- backend/judge0.js
+++ backend/judge0.js
@@ -1,8 +1,11 @@
 const axios=require("axios");
 const languageIds={
-  cpp:54,python:71,java:62,javascript:63
+  cpp:54,
+  python:71,
+  java:62,
+  javascript:63
 };
-async function runCode(language,code){
+async function runCode(language,code,input=""){
   const languageId=languageIds[language];
   if(!languageId){
     throw new Error("Unsupported language");
@@ -14,27 +17,25 @@ async function runCode(language,code){
     `${process.env.JUDGE0_URL}/submissions?base64_encoded=false`,
     {
       language_id:languageId,
-      source_code:code
+      source_code:code,
+      stdin:input
     },
     {headers}
   );
   const token=submission.data.token;
   for(let i=0;i<30;i++){
     await new Promise(resolve=>setTimeout(resolve,1000));
-    const response=await axios.get(
-      `${process.env.JUDGE0_URL}/submissions/${token}?base64_encoded=false`,
-      {headers}
-    );
+    const response=await axios.get(`${process.env.JUDGE0_URL}/submissions/${token}?base64_encoded=false`,{headers});
     const result=response.data;
-    if(result.status && result.status.id>=3){
+    if(result.status&&result.status.id>=3){
       return{
-        output:
-          result.stdout || result.stderr || result.compile_output || result.message || "No output",
-          time:result.time || "0", memory:result.memory || "0", status:result.status.description || "Unknown"
+        output:result.stdout||result.stderr||result.compile_output||result.message||"No output",
+        time:result.time||"0",
+        memory:result.memory||"0",
+        status:result.status.description||"Unknown"
       };
     }
   }
   throw new Error("Execution timed out");
 }
 module.exports=runCode;
-
